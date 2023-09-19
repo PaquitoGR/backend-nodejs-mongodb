99819c72f1b8cadc2ec8f53db0428424c29d61f0
Francisco Suárez <paco.max@gmail.com>
2023-09-19 20:39:12 +0200

    add modules folder

---
+++ ejemplos/modulos/calculadora.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("Inicializo la calculadora");
+
+function suma(a, b) {
+  return a + b;
+}
+
+module.exports.suma = suma;
+// ALIAS -> exports.suma = suma;
+
+// module.exports = suma;
