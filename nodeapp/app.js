b6c008c019cf4ddc119c3ba9bbeba580ae42e651
Francisco Suárez <paco.max@gmail.com>
2023-09-21 20:51:38 +0200

    added ejs examples to nodeapp

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+//app.use('pdfs', express.static(path.join(__dirname, "/pdfs")));
 
 // app.use((req, res, next) => {
 //   console.log("Ha llegado una petición a", req.url);
--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.locals.texto = "Hola";
+  res.locals.nombre = '<script>alert("inyección de código")</script>';
+
+  const ahora = new Date();
+  res.locals.esPar = ahora.getSeconds() % 2 === 0;
+  res.locals.segundoActual = ahora.getSeconds();
+
+  res.locals.usuarios = [
+    { nombre: "Smith", edad: 32 },
+    { nombre: "Jones", edad: 27 },
+  ];
+
+  res.render("index");
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
