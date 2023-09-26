fd4d01a9e5e2603e7bee612a2e78c1fa315c55fe
Francisco Suárez <paco.max@gmail.com>
2023-09-26 23:18:09 +0200

    axios test

---
+++ ejemplos/ejemplo-request/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const axios = require('axios');
+
+main().catch(err => console.log('Hubo un error', err));
+
+async function main() {
+
+  const response = await axios.get('https://swapi.dev/api/people/');
+  //const response = await axios.get('http://127.0.0.1:3000/api/agentes');
+
+  console.log(response);
+}
