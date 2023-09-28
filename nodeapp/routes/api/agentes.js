e7b669a50f3b80caf03bda51c6fd373267b756d2
Francisco Suárez <paco.max@gmail.com>
2023-09-28 20:23:41 +0200

    added basic-auth

--- nodeapp/routes/api/agentes.js
+++ nodeapp/routes/api/agentes.js
@@ -34,9 +34,9 @@ router.get('/', async (req, res, next) => {
     const agentes = await Agente.lista(filtro, skip, limit, sort, fields);
     
     // probamos un método de instancia
-    agentes.forEach(agente => {
-      agente.saluda();
-    });
+    // agentes.forEach(agente => {
+    //   agente.saluda();
+    // });
 
     res.json({ results: agentes })
 
